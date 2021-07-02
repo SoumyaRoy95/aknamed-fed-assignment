ad206714b34b74f3fe3b6b82d68c7f889f4cefca
SoumyaRoy95 <soumyoroy3@gmail.com>
2021-07-02 18:38:15 +0530

    the project UI is completed

--- js/details.js
+++ js/details.js
@@ -19,3 +19,23 @@ function descload() {
     document.getElementById("desctab").style.borderBottom = "2px solid blue"
     document.getElementById("subcontent").style.display = "flex"
 }
+
+function modal_open(){
+    const modal = document.querySelector('.modal')
+    modal.classList.add('modal-open')
+}
+
+function modal_close(){
+    const modal = document.querySelector('.modal')
+    modal.classList.remove('modal-open')
+}
+
+function sidebar(){
+    const container = document.querySelector('.container');
+    container.classList.add("sidebar-open")
+}
+
+function overlay(){
+    const container = document.querySelector('.container');
+    container.classList.remove("sidebar-open")
+}
--- js/navbar.js
+++ js/navbar.js
@@ -19,6 +19,16 @@ function overlay(){
     const dashboard = document.querySelector('.dashboard');
     dashboard.classList.remove("sidebar-open")
 }
+
+function sidebar_products(){
+    const products_container = document.querySelector('.products-container');
+    products_container.classList.add("sidebar-open")
+}
+
+function overlay_products(){
+    const products_container = document.querySelector('.products-container');
+    products_container.classList.remove("sidebar-open")
+}
         
 
 
