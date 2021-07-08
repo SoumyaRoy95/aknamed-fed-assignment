ee828aa393106ff1d687585e9f88d7cbf210ccb0
SoumyaRoy95 <soumyoroy3@gmail.com>
2021-07-08 18:57:58 +0530

    optimized code

--- js/login.js
+++ js/login.js
@@ -4,7 +4,7 @@ function login(){
     if(username==="testuser" && password==="test"){
         window.location.href="dashboard.html"
     } else {
-        document.getElementById("wrong-creds").style.display = "flex"
+        document.getElementById("wrong-creds").style.visibility = "visible"
         document.getElementById("username").value = ""
         document.getElementById("password").value = ""
     }
